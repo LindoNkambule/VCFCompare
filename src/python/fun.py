1b85f9c012e19acd90c6f0c31333c99c42267217
LindoNkambule <lindonkambule116@gmail.com>
2019-11-03 21:45:55 +0200

    switched from list comprehensions to sets in the variantCalls() function for speed

--- src/python/fun.py
+++ src/python/fun.py
@@ -9,7 +9,9 @@ def vcfDFtoList(vcfdf):
     return vcfList
     #convert df to a list
 
-def snvINDELlists(snvList, indelList, variantsList):
+def snvINDELlists(variantsList):
+    snvList = []
+    indelList = []
     for variant in variantsList:
         ref = len(str(variant[2]))
         alt = len(str(variant[3]))
@@ -17,11 +19,14 @@ def snvINDELlists(snvList, indelList, variantsList):
             indelList.append(variant)
         else:
             snvList.append(variant)
+    return snvList, indelList
     #separate SNVs and INDELs into separate lists
     #this function takes (1) two empty SNV and INDEL lists and (2) a list with variants, and separates the variants according to size (SNVs and INDELs)
 
 def variantCalls(truth, query):
-    TPs = [x for x in query if x in truth]
-    FPs = [x for x in query if x not in truth]
-    FNs = [x for x in truth if x not in query]
+    Truth_set = set(map(tuple, truth)) #convert nested lists to sets for speed
+    Query_Set = set(map(tuple, query))
+    TPs = Truth_set.intersection(Query_Set)
+    FPs = Query_Set.difference(Truth_set)
+    FNs = Truth_set.difference(Query_Set)
     return TPs, FPs, FNs
